51be9f9a9bb6b15e8dd1ce4e683e52aa39debe34
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-11-21 02:05:44 +0300

    Placeholders removed and replaced with actual values

--- awards/views.py
+++ awards/views.py
@@ -14,9 +14,12 @@ from django.db.models import Max
 # Create your views here.
 def index(request):
     date = dt.date.today()
-    winners=Project.objects.all()
+    winners=Project.objects.all()[:4]
     caraousel = Project.objects.order_by('-overall_score')[0]
-    donaldkiplagat='donaldkiplagat'
+    nominees=Project.objects.all()[4:8]
+    directories=Project.objects.all()[8:11]
+    resources=Project.objects.all()[11:15]
+    resources2=Project.objects.all()[15:19]
 
     try:
         if not request.user.is_authenticated:
@@ -27,7 +30,7 @@ def index(request):
     except ObjectDoesNotExist:
         return redirect('create-profile')
 
-    return render(request,'index.html',{"winners":winners,"profile":profile,"caraousel":caraousel,"date":date})
+    return render(request,'index.html',{"winners":winners,"profile":profile,"caraousel":caraousel,"date":date,"nominees":nominees,"directories":directories,"resources":resources,"resources2":resources2})
 
 def create_profile(request):
     current_user = request.user
