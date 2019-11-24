98d402b207d98bfea3009578c4145bd821e76797
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-24 12:15:26 +0300

    register technologies class

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from.models import categories
+from.models import categories,technologies,colors,countries,Project,Profile,Rating
 class ProjectAdmin(admin.ModelAdmin):
     filter_horizontal =('technologies','categories','colors')
 # Register your models here.
 admin.site.register(categories)
+admin.site.register(technologies)
