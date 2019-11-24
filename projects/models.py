03c972ec1854bc4d434d80d68c9c01980458ff66
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-24 09:08:39 +0300

    Add categories model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
+import datetime as dt 
+
 
 # Create your models here.
+class categories(models.Model):
+    categories= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categories
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def delete_category(cls,categories):
+        cls.objects.filter(categories=categories).delete()
