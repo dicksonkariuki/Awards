a9e0f4ec5b966a7bb7f74bae153557eca07937c3
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-24 12:14:07 +0300

    Add filtering of projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from.models import categories
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal =('technologies','categories','colors')
 # Register your models here.
 admin.site.register(categories)
