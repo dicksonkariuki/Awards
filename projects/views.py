e50c61a21c1f2c0e14c55fe8f2c7f2ddc5ab15fa
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-24 12:03:31 +0300

    Add view function to display profile

--- projects/views.py
+++ projects/views.py
@@ -24,3 +24,69 @@ def index(request):
     directories=Project.objects.all()[8:11]
     resources=Project.objects.all()[11:15]
     resources2=Project.objects.all()[15:19]
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile =Profile.objects.get(username=current_user)
+        print(current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request,'index.html',{"winners":winners,"profile":profile,"caraousel":caraousel,"date":date,"nominees":nominees,"directories":directories,"resources":resources,"resources2":resources2})
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method=='POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+
+            profile.save()
+        return redirect('Index')
+    else:
+        form=ProfileForm()
+    return render(request,'create_profile.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
+    if request.method =='POST':
+        form = ProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.username = current_user
+            project.avatar = profile.avatar
+            project.country = profile.country
+
+            project.save()
+    else:
+        form = ProjectForm()
+
+    return render(request,'new_project.html',{"form":form})
+
+def directory(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
+
+    winners=Project.objects.all()
+    caraousel = Project.objects.get(id=8)
+
+    return render(request,'directory.html',{"winners":winners,"profile":profile,"caraousel":caraousel,"date":date})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
+    projects=Project.objects.filter(username=current_user)
+
+    return render(request,'profile.html',{"projects":projects,"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def site(request,site_id):
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
