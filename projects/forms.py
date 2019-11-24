dda0aea5e2056150e11346f6b8719727a579d3e9
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-24 12:13:03 +0300

    Add rating form

--- projects/forms.py
+++ projects/forms.py
@@ -14,3 +14,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model=Rating
+        exclude=['overall_score','profile','project']
