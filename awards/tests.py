7cf0b815f5711031c9d0666b086337d0ed45dbd5
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-27 11:09:29 +0300

    Add categories testclass

--- awards/tests.py
+++ awards/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import categories,technologies,colors,countries
+from .models import *
 from django.contrib.auth.models import User
 import datetime as dt
 # Create your tests here.
@@ -23,50 +23,6 @@ class categoriesTestClass(TestCase):
         category = categories.objects.all()
         self.assertTrue(len(category)==0)
 
-class technologiesTestClass(TestCase):
-    def setUp(self):
-        self.Python = technologies(technologies='Python')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.Python,technologies))
-
-    def tearDown(self):
-        technologies.objects.all().delete()
-
-    def test_save_method(self):
-        self.Python.save_technology()
-        technology = technologies.objects.all()
-        self.assertTrue(len(technology)>0)
-
-    def test_delete_method(self):
-        self.Python.delete_technology('Python')
-        technology = technologies.objects.all()
-        self.assertTrue(len(technology)==0)
-
-
-class countriesTestClass(TestCase):
-    def setUp(self):
-        self.Kenya = countries(countries='Kenya')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.Kenya,countries))
-
-    def tearDown(self):
-        countries.objects.all().delete()
-
-    def test_save_method(self):
-        self.Kenya.save_country()
-        country = countries.objects.all()
-        self.assertTrue(len(country)>0)
-
-    def test_delete_method(self):
-        self.Kenya.delete_country('Kenya')
-        country = countries.objects.all()
-        self.assertTrue(len(country)==0)
-
-
-
-
 class colorsTestClass(TestCase):
     def setUp(self):
         self.Black = colors(colors='Black')
@@ -87,42 +43,3 @@ class colorsTestClass(TestCase):
         color = colors.objects.all()
         self.assertTrue(len(color)==0)
 
-# class LocationTestClass(TestCase):
-#     def setUp(self):
-#         self.Moringa = Location(location='Moringa')
-#
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Moringa,Location))
-#
-#     def tearDown(self):
-#         Location.objects.all().delete()
-#
-#     def test_save_method(self):
-#         self.Moringa.save_location()
-#         locations = Location.objects.all()
-#         self.assertTrue(len(locations)>0)
-#
-#     def test_delete_method(self):
-#         self.Moringa.delete_location('Moringa')
-#         locations = Location.objects.all()
-#         self.assertTrue(len(locations)==0)
-#
-# class LocationTestClass(TestCase):
-#     def setUp(self):
-#         self.Moringa = Location(location='Moringa')
-#
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.Moringa,Location))
-#
-#     def tearDown(self):
-#         Location.objects.all().delete()
-#
-#     def test_save_method(self):
-#         self.Moringa.save_location()
-#         locations = Location.objects.all()
-#         self.assertTrue(len(locations)>0)
-#
-#     def test_delete_method(self):
-#         self.Moringa.delete_location('Moringa')
-#         locations = Location.objects.all()
-#         self.assertTrue(len(locations)==0)
