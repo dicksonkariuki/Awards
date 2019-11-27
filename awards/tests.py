2af9969a42af87570cfcfbd0c0c832ff67b1375f
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-27 11:12:33 +0300

    Add test for detele color method

--- awards/tests.py
+++ awards/tests.py
@@ -5,21 +5,21 @@ import datetime as dt
 # Create your tests here.
 class categoriesTestClass(TestCase):
     def setUp(self):
-        self.Art = categories(categories='Art')
+        self.photos = categories(categories='Art')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.Art,categories))
+        self.assertTrue(isinstance(self.photos,categories))
 
     def tearDown(self):
         categories.objects.all().delete()
 
     def test_save_method(self):
-        self.Art.save_category()
+        self.photos.save_category()
         category = categories.objects.all()
         self.assertTrue(len(category)>0)
 
     def test_delete_method(self):
-        self.Art.delete_category('Art')
+        self.photos.delete_category('photos')
         category = categories.objects.all()
         self.assertTrue(len(category)==0)
 
